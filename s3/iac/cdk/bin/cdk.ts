7b29f0d363c042c5d729e634f280f06b37981512
dtan0927 <davidtan950@gmail.com>
2025-04-02 23:39:01 +0000

    simple s3 cdk example

---
+++ s3/iac/cdk/bin/cdk.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+new CdkStack(app, 'CdkStack', {
+    env: { region: 'us-east-1' }
+  });
