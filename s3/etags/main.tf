aee1d0efc962a7f4d97771a10a423842d1eb8662
dtan0927 <davidtan950@gmail.com>
2025-04-03 17:07:10 +0000

    etag work

---
+++ s3/etags/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "default" {
+    bucket = "my-unique-bucket-name-123456"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = "aws_s3_bucket.default.bucket"
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  #etag = filemd5("myfile.txt")
+  
+  depends_on = [aws_s3_bucket.default]  # Ensures the bucket is fully available
+}
